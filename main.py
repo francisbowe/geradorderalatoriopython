934e085519da11e411acfd07f2694fc576db82bf
francisbowe <franciscodomingos20155@gmail.com>
2025-03-13 12:15:15 +0100

    adiciona funcionalidade de envio de e-mail com yagmail e validação de credenciais

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import pandas as pd
+import yagmail
+from dotenv import load_dotenv
 
 
 #importar Base Dados
 # Get the file path from an environment variable
 file_path = os.getenv('VENDAS_FILE_PATH', 'Vendas.xlsx')
 tabela_vendas = pd.read_excel(file_path)
-print(tabela_vendas)
+#print(tabela_vendas)
 
 #visualizar a base de dados
 pd.set_option('display.max_columns', None)
@@ -24,5 +26,24 @@ media_ticket = media_ticket.rename(columns={0: 'Ticket Médio'})
 #print(media_ticket)
 
 #enviar e-mail para diretoria com resumo do faturamento por loja
+load_dotenv ()
 
-email = input('Qual o seu e-mail?')
+# Obtém credenciais do ambiente
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+# Verifica se as credenciais foram carregadas
+if not EMAIL_USER or not EMAIL_PASSWORD:
+    raise ValueError("Erro: EMAIL_USER ou EMAIL_PASSWORD não definidos corretamente!")
+
+# Configura o yagmail
+yag = yagmail.SMTP(EMAIL_USER, EMAIL_PASSWORD)
+
+# Envia e-mail
+yag.send(
+    to="franciscobowe10@gmail.com",
+    subject="Teste de E-mail",
+    contents="Este é um teste de envio de e-mail usando yagmail no macOS!"
+)
+
+print("E-mail enviado com sucesso!")
